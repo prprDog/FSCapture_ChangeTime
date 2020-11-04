6050f2891acae47a20b2ed7e639af7af5c748450
lgq <583754440@qq.com>
2020-11-04 19:57:21 +0800

    修改执行路径

--- FSCapture_ChangeTime/FSCapture_ChangeTime.cpp
+++ FSCapture_ChangeTime/FSCapture_ChangeTime.cpp
@@ -49,7 +49,7 @@ char* converToArray(WORD result) {
 
 // 写回文件
 void saveToFile(WORD result) {
-    char path[MAX_PATH] = "F:\\Project\\DeskTop\\fsc.db";
+    char path[MAX_PATH] = ".\\fsc.db";
     HANDLE hfile = CreateFileA(path, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (GetLastError() == ERROR_FILE_NOT_FOUND)
     {
@@ -73,15 +73,6 @@ void saveToFile(WORD result) {
         return;
     }
     CloseHandle(hfile);
+    cout << "试用时间修改成功" << endl;
+    system("pause");
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
